7aa1a259d279b268812e4dfee2c363fc27085e9f
William <wrs20@bath.ac.uk>
2015-03-04 11:24:19 +0000

    main changes

--- test2/src/main.py
+++ test2/src/main.py
@@ -9,33 +9,34 @@ import math
 if __name__ == '__main__':
     
     
-    print "test MD"
+    test_1000 = True
+    test_2_bounce = False
     
-    n=10
-    N=n**3
-    rho = 3.2
+    if (test_1000):
+        n=10
+        N=n**3
+        rho = 3.2
+        mu = 0.0
+        nsig = 2.5
+        
+        test_domain = domain.BaseDomain()
+        test_potential = potential.LennardJones(sigma=1.0,epsilon=1.0)    
     
-    #N=2
+        test_pos_init = state.PosInitLatticeNRho(N, rho)
+        test_vel_init = state.VelInitNormDist(mu,nsig)    
     
-    
-    
-    mu = 0.0
-    nsig = 2.5
-    
-    
-    
-    
-    
-    test_domain = domain.BaseDomain()
-    test_potential = potential.LennardJones(sigma=1.0,epsilon=1.0)
-    
-    test_pos_init = state.PosInitLatticeNRho(N, rho)
-    test_vel_init = state.VelInitNormDist(mu,nsig)
-    
-    #test_pos_init = state.PosInitTwoParticlesInABox(rx = 0.5, extent = np.array([7., 7., 7.]), axis = np.array([1,0,0]))
-    #test_vel_init = state.VelInitTwoParticlesInABox(vx = np.array([0., 0., 0.]), vy = np.array([0., 0., 0.]))
-    
-    r=0.5
+    if (test_2_bounce):
+        N=2
+        
+        test_domain = domain.BaseDomain()
+        test_potential = potential.LennardJones(sigma=1.0,epsilon=1.0)
+        
+        test_pos_init = state.PosInitTwoParticlesInABox(rx = 0.5, extent = np.array([7., 7., 7.]), axis = np.array([1,0,0]))
+        test_vel_init = state.VelInitTwoParticlesInABox(vx = np.array([0., 0., 0.]), vy = np.array([0., 0., 0.]))
+        
+        
+
+    #r=0.5
     #test_pos_init = state.PosInitTwoParticlesInABox(rx = r, extent = np.array([7., 7., 7.]), axis = np.array([1,0,0]))
     #test_vel_init = state.VelInitTwoParticlesInABox(vx = np.array([0., 1./(math.sqrt(2*r)), 0.]), vy = np.array([0., -1./(math.sqrt(2*r)), 0.]))
     
