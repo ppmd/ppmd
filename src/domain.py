d3349efa26c085ad32ef8a9b180b5fbb1540f55f
William Saunders <wrs20@bath.ac.uk>
2015-04-06 01:02:41 +0100

    domain.py methods removed

--- src/domain.py
+++ src/domain.py
@@ -50,59 +50,7 @@ class BaseDomain():
         """
         return self._cell_count
     
-    '''    
-    def cell_index_lin(self,C_in):
-        """
-        Convert tuple index to linear index. Applies periodic boundaries first.
-        
-        :arg int c_in: Input index to convert.
-        """
-        
-        C_in[0] = (C_in[0]-1) % self._cell_array[0] + 1
-        C_in[1] = (C_in[1]-1) % self._cell_array[1] + 1        
-        C_in[2] = (C_in[2]-1) % self._cell_array[2] + 1    
-        
-
-        return ((C_in[2] - 1)*self._cell_array[1] + C_in[1] - 1)*self._cell_array[0] + C_in[0]
-    '''
     
-    '''    
-    def cell_index_lin_offset(self,C_in_abs):
-        """
-        Convert tuple index to linear index. Applies periodic boundaries first. Returns periodic boundary offsets.
-        
-        :arg np.array(3,1) c_in_abs: Input index to convert.
-        """
-        
-        
-        C_in = np.zeros(3,dtype=ctypes.c_int)
-        C_in[0] = (C_in_abs[0]-1) % self._cell_array[0] + 1
-        C_in[1] = (C_in_abs[1]-1) % self._cell_array[1] + 1
-        C_in[2] = (C_in_abs[2]-1) % self._cell_array[2] + 1
-        
-        correction = np.array([((C_in[2] - 1)*self._cell_array[1] + C_in[1] - 1)*self._cell_array[0] + C_in[0],0,0,0,0],dtype=ctypes.c_int)
-        
-        if (C_in[0] != C_in_abs[0] or C_in[1] != C_in_abs[1] or C_in[2] != C_in_abs[2]):
-            correction[4] = 1
-            correction[1:4:1] = np.sign(C_in_abs-C_in)
-         
-
-        return correction
-    '''   
-    
-    '''    
-    def cell_index_tuple(self,c_in):
-        """
-        Convert cell linear index to vector.
-        
-        :arg int c_in: Input index.
-        """    
-        Cz = 1 + (c_in-1)/(self._cell_array[0]*self._cell_array[1])
-        Cx = 1 + (c_in-1) % self._cell_array[0]
-        Cy = 1 + int((c_in - (Cz-1)*(self._cell_array[0]*self._cell_array[1]) -1)/(self._cell_array[0]))
-
-        return np.array([Cx,Cy,Cz])
-    '''    
     
     # Used in creation of cell link list.    
     def get_cell_lin_index(self,r_in):
