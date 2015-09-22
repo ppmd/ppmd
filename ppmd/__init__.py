69d6308dea982a98ed7f4c18e321a19566e37761
William <wrs20@bath.ac.uk>
2015-09-22 10:59:43 +0100

    added __init__.py

---
+++ ppmd/__init__.py
@@ -0,0 +1,49 @@
+
+
+
+__all__ = [ 'access',
+            'build',
+            'cell',
+            'data',
+            'domain',
+            'gpucuda',
+            'halo',
+            'host',
+            'kernel',
+            'loop',
+            'main',
+            'method',
+            'move',
+            'mpi',
+            'pairloop',
+            'pio',
+            'potential',
+            'runtime',
+            'simulation',
+            'state']
+
+
+
+
+
+
+import access
+import build
+import cell
+import data
+import domain
+import gpucuda
+import halo
+import host
+import kernel
+import loop
+import main
+import method
+import move
+import mpi
+import pairloop
+import pio
+import runtime
+import simulation
+import state
+
