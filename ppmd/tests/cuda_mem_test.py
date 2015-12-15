d9f425b44c9795ef8ee9a4450bb3162e05c9b16d
William <wrs20@bath.ac.uk>
2015-12-15 14:57:42 +0000

    indents...

--- ppmd/tests/cuda_mem_test.py
+++ ppmd/tests/cuda_mem_test.py
@@ -6,11 +6,11 @@ import numpy as np
 from ppmd.cuda import *
  
 
- cuda_runtime.cuda_mem_get_info()
+cuda_runtime.cuda_mem_get_info()
 
- d_a1 = cuda_base.Array(np.array(range(10)))
- d_a2 = cuda_base.Array(ncomp=1000)
+d_a1 = cuda_base.Array(np.array(range(10)))
+d_a2 = cuda_base.Array(ncomp=1000)
 
 
- d_b = cuda_base.Matrix(initial_value=np.array([[1, 2], [3, 4]]))
- d_c = cuda_base.Matrix(nrow=10000, ncol=10000)
+d_b = cuda_base.Matrix(initial_value=np.array([[1, 2], [3, 4]]))
+d_c = cuda_base.Matrix(nrow=10000, ncol=10000)
