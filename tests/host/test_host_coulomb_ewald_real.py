e01ddd76b0b97db728fb8101b4d4d21b6c776127
William <wrs20@bath.ac.uk>
2017-04-30 15:03:09 +0100

    fixed ewald tests

--- tests/host/test_host_coulomb_ewald_real.py
+++ tests/host/test_host_coulomb_ewald_real.py
@@ -113,14 +113,14 @@ def test_ewald_energy_python_nacl_2():
     e = 30.0
     domain = md.domain.BaseDomainHalo(extent=(e,e,e))
     c = md.coulomb.CoulombicEnergy(domain=domain, real_cutoff=12.)
-    assert abs(c.recip_cutoff - 0.28601) < 10.**-1, "recip space cutoff"
+    assert abs(c.recip_cutoff - 0.28601*scipy.constants.pi*2.0) < 10.**-1, "recip space cutoff"
     assert abs(c.real_cutoff - 12.) < 10.**-15., "real space cutoff"
     assert c.kmax[0] == 9, "kmax_x"
     assert c.kmax[1] == 9, "kmax_y"
     assert c.kmax[2] == 9, "kmax_z"
-    assert abs(c.recip_vectors[0][0] - 0.0333333) < 10.**-5, "xrecip vector"
-    assert abs(c.recip_vectors[1][1] - 0.0333333) < 10.**-5, "yrecip vector"
-    assert abs(c.recip_vectors[2][2] - 0.0333333) < 10.**-5, "zrecip vector"
+    assert abs(c.recip_vectors[0][0] - 0.0333333*2.*scipy.constants.pi) < 10.**-5, "xrecip vector"
+    assert abs(c.recip_vectors[1][1] - 0.0333333*2.*scipy.constants.pi) < 10.**-5, "yrecip vector"
+    assert abs(c.recip_vectors[2][2] - 0.0333333*2.*scipy.constants.pi) < 10.**-5, "zrecip vector"
 
 def test_ewald_energy_python_co2_2():
     """
@@ -134,14 +134,14 @@ def test_ewald_energy_python_co2_2():
     e = 24.4750735
     domain = md.domain.BaseDomainHalo(extent=(e,e,e))
     c = md.coulomb.CoulombicEnergy(domain=domain, real_cutoff=12.)
-    assert abs(c.recip_cutoff - 0.28601) < 10.**-1, "recip space cutoff"
+    assert abs(c.recip_cutoff - 0.28601*scipy.constants.pi*2.0) < 10.**-1, "recip space cutoff"
     assert abs(c.real_cutoff - 12.) < 10.**-15., "real space cutoff"
     assert c.kmax[0] == 7, "kmax_x"
     assert c.kmax[1] == 7, "kmax_y"
     assert c.kmax[2] == 7, "kmax_z"
-    assert abs(c.recip_vectors[0][0] - 0.0408579) < 10.**-5, "xrecip vector"
-    assert abs(c.recip_vectors[1][1] - 0.0408579) < 10.**-5, "yrecip vector"
-    assert abs(c.recip_vectors[2][2] - 0.0408579) < 10.**-5, "zrecip vector"
+    assert abs(c.recip_vectors[0][0] - 0.0408579*2.*scipy.constants.pi) < 10.**-5, "xrecip vector"
+    assert abs(c.recip_vectors[1][1] - 0.0408579*2.*scipy.constants.pi) < 10.**-5, "yrecip vector"
+    assert abs(c.recip_vectors[2][2] - 0.0408579*2.*scipy.constants.pi) < 10.**-5, "zrecip vector"
 
 
 
