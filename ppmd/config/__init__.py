c94ed3178a6e66afc44322bdcd7f6fdd5b7b416f
William <wrs20@bath.ac.uk>
2017-07-20 17:13:52 +0100

    py3 progress

--- ppmd/config/__init__.py
+++ ppmd/config/__init__.py
@@ -3,4 +3,4 @@ from __future__ import print_function, division
 __author__ = "W.R.Saunders"
 __copyright__ = "Copyright 2016, W.R.Saunders"
 
-from ppmd.config.config import MAIN_CFG, COMPILERS
+from ppmd.config.config import MAIN_CFG, COMPILERS, str_to_bool
