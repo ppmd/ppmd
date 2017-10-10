fixed cube mid calculation bug

tests/host/test_host_fmm_1.py
@@ -44,7 +44,8 @@ def test_fmm_init_1():
     A.Q[:] -= bias
     A.scatter_data_from(0)
 
-    fmm = PyFMM(domain=A.domain, N=1000)
+    fmm = PyFMM(domain=A.domain, N=N)
+    print("cube_side_len", 2**(fmm.R-1), "extent", E)
     fmm._compute_cube_contrib(A.P, A.Q)
 
     print(np.sum(fmm.entry_data[:]))