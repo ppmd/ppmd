bd7dd5ba401fb82919731929fc91a6e9c2946fc5
William <wrs20@bath.ac.uk>
2019-04-30 11:25:19 +0100

    added setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-with open("README", 'r') as f:
-    long_description = f.read()
-
 long_description = """PPMD is a portable high level framework to create high performance Molecular Dynamics codes. The principle idea is that a simulation consists of sets of particles and most operations on these particles can be described using either a loop over all particles or a loop over particle pairs and applying some operation.
 """
 
