653ff660e4170cbd1851b90ca7401f739eaa89c7
William <wrs20@bath.ac.uk>
2019-04-30 11:24:08 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+long_description = """PPMD is a portable high level framework to create high performance Molecular Dynamics codes. The principle idea is that a simulation consists of sets of particles and most operations on these particles can be described using either a loop over all particles or a loop over particle pairs and applying some operation.
+"""
+
+install_requires = []
+with open('requirements.txt') as fh:
+    l = fh.readline()
+    if len(l) > 0:
+        install_requires.append(l)
+
+setup(
+   name='ppmd',
+   version='1.0',
+   description='Performance Portable Molecular Dynamics',
+   license="GPL3",
+   long_description=long_description,
+   author='William R Saunders',
+   author_email='W.R.Saunders@bath.ac.uk',
+   url="https://bitbucket.org/wrs20/ppmd",
+   packages=['ppmd'],
+   install_requires=install_requires,
+   scripts=[]
+)
